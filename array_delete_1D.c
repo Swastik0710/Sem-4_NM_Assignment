3a1a44454ec192f2d4ee3add2af80ac81d10a1e3
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-02-07 12:42:17 +0530

    Create array_delete_1D.c

---
+++ array_delete_1D.c
@@ -0,0 +1,33 @@
+////Write a program to delete an element in an array at any position.
+#include<stdio.h>
+int main() {
+  int size, pos;
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  int arr[size];
+  printf("Enter %d elements: \n", size);
+  for (int i = 0; i < size; i++) {
+      scanf("%d", &arr[i]);
+  }
+  printf("The elements of the array are: \n");
+  for (int i = 0; i < size; i++) {
+      printf("%d ", arr[i]);
+  }
+  printf("\n");
+  printf("Enter the position to delete the element: ");
+  scanf("%d", &pos);
+  if (pos < 0 || pos > size) {
+      printf("Invalid position !");
+      return 1;
+  }
+  for (int i = pos; i < size-1; i++) {
+      arr[i] = arr[i+1];
+  }
+  size--;
+  printf("The updated array after deletion is: \n");
+  for (int i = 0; i < size; i++) {
+      printf("%d ", arr[i]);
+  }
+  printf("\n");
+  return 0;
+}
