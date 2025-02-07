df53223ca7b099839a3b8953435c27c8820dfae2
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2025-02-07 12:28:42 +0530

    Create array_1D.c

---
+++ array_1D.c
@@ -0,0 +1,18 @@
+//Write a program to display the elements of 1D array.
+#include<stdio.h>
+int main() {
+  int size;
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  int arr[size];
+  printf("Enter %d elements: \n", size);
+  for (int i = 0; i < size; i++) {
+      scanf("%d", &arr[i]);
+  }
+  printf("The elements of the array are: \n");
+  for (int i = 0; i < size; i++) {
+      printf("%d ", arr[i]);
+  }
+  printf("\n");
+  return 0;
+}
